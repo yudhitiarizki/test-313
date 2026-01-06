adaf8d2659f39764882abcf2e18e247c2d93a118
skipper <skipper@rimbahouse.com>
2026-01-06 22:27:22 +0700

    add feature update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001/api";
+const API_BASE_URL = "https://backend-test-313.up.railway.app/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
